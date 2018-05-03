060ca3c7681d932add0be2a3580cc629c9ddb532
yusei-wy <yusei.kasa@gmail.com>
2018-05-04 07:14:58 +0900

    Add ヘッダファイル整備(haribO3c)

--- chapter06/graphic.c
+++ chapter06/graphic.c
@@ -2,41 +2,7 @@
  * グラフィック処理関係
  */
 
-extern void io_hlt(void);
-extern void io_cli(void);
-extern int io_out8(int port, int data);
-extern int io_load_eflags(void);
-extern void io_store_eflags(int eflags);
-
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
-              int x1, int y1);
-// TODO: vram, xsize, ysize をもつ構造体を用意する
-void init_screen8(char *vram, int x, int y);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c,
-                   unsigned char *s);
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
-
-// パレットを簡単に扱うための定数
-#define COL8_000000 0
-#define COL8_FF0000 1
-#define COL8_00FF00 2
-#define COL8_FFFF00 3
-#define COL8_0000FF 4
-#define COL8_FF00FF 5
-#define COL8_00FFFF 6
-#define COL8_FFFFFF 7
-#define COL8_C6C6C6 8
-#define COL8_840000 9
-#define COL8_008400 10
-#define COL8_848400 11
-#define COL8_000084 12
-#define COL8_840084 13
-#define COL8_008484 14
-#define COL8_848484 15
+#include "bootpack.h"
 
 /**
  * パレットを作成
