cbf3187ee651f812b9c91db4b1e9524c02c88a0d
yusei-wy <yusei.kasa@gmail.com>
2018-06-08 09:04:09 +0900

    Add タイムアウト機能(haribO9c)

--- chapter12/bootpack.c
+++ chapter12/bootpack.c
@@ -5,9 +5,10 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
 
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
-  char s[40], keybuf[32], mousebuf[128];
+  struct FIFO8 timerfifo;
+  char s[40], keybuf[32], mousebuf[128], timerbuf[8];
   int mx, my, i;
-  unsigned int memtotal, count = 0;
+  unsigned int memtotal;
   struct MOUSE_DEC mdec;
   struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
   struct SHTCTL *shtctl;
@@ -22,6 +23,9 @@ void HariMain(void) {
   init_pit();
   io_out8(PIC0_IMR, 0xf8); // PIT と PTC1 とキーボードを許可
   io_out8(PIC1_IMR, 0xef); // マウスを許可(11101111)
+
+  fifo8_init(&timerfifo, 8, timerbuf);
+  settimer(1000, &timerfifo, 1);
 
   init_keyboard();
   enable_mouse(&mdec);
@@ -59,14 +63,13 @@ void HariMain(void) {
   sheet_refresh(sht_back, 0, 0, binfo->scrnx, 48);
 
   for (;;) {
-    count++;
-    sprintf(s, "%d", count);
+    sprintf(s, "%d", timerctl.count);
     boxfill8(buf_win, 160, COL8_C6C6C6, 40, 28, 119, 43);
     putfonts8_asc(buf_win, 160, 40, 28, COL8_000000, s);
     sheet_refresh(sht_win, 40, 28, 120, 44);
 
     io_cli(); // 割り込み禁止
-    if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
+    if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) == 0) {
       io_sti();
     } else {
       if (fifo8_status(&keyfifo) != 0) {
@@ -108,6 +111,11 @@ void HariMain(void) {
           sheet_refresh(sht_back, 0, 0, 80, 16);
           sheet_slide(sht_mouse, mx, my); // sheet_refresh を含む
         }
+      } else if (fifo8_status(&timerfifo) != 0) {
+        i = fifo8_get(&timerfifo);  // とりあえず読み込む 
+        io_sti();
+        putfonts8_asc(buf_back, binfo->scrnx, 0, 64, COL8_FFFFFF, "10[sec]");
+        sheet_refresh(sht_back, 0, 64, 56, 80);
       }
     }
   }
