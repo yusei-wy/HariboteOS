ff7936cccef839ccfbd71cbe7eb6f150cdcf8dab
yusei-wy <yusei.kasa@gmail.com>
2018-06-18 21:22:51 +0900

    Fix PIC1 を IRQ2 に接続する記述を間違っていた

--- chapter13/int.c
+++ chapter13/int.c
@@ -9,21 +9,21 @@
  * PIC の初期化
  */
 void init_pic(void) {
-  io_out8(PIC0_IMR, 0xff);  // 全ての割り込みを受け付けない
-  io_out8(PIC1_IMR, 0xff);  // 全ての割り込みを受け付けない
+  io_out8(PIC0_IMR, 0xff);    // 全ての割り込みを受け付けない
+  io_out8(PIC1_IMR, 0xff);    // 全ての割り込みを受け付けない
 
-  io_out8(PIC0_ICW1, 0x11); // エッジトリガモード
-  io_out8(PIC0_ICW2, 0x20); // IRQ0-7 は INT20-27 で受け付ける
+  io_out8(PIC0_ICW1, 0x11);   // エッジトリガモード
+  io_out8(PIC0_ICW2, 0x20);   // IRQ0-7 は INT20-27 で受け付ける
   io_out8(PIC0_ICW3, 1 << 2); // PIC1 は IRQ2 にて接続
-  io_out8(PIC0_ICW4, 0x01); // ノンバッファモード
+  io_out8(PIC0_ICW4, 0x01);   // ノンバッファモード
 
-  io_out8(PIC1_ICW1, 0x11); // エッジトリガモード
-  io_out8(PIC1_ICW2, 0x28); // IRQ8-15 は INT28-2f で受け付ける
-  io_out8(PIC1_ICW3, 3);    // PIC1 は IRQ2 にて接続
-  io_out8(PIC1_ICW4, 0x01); // ノンバッファモード
+  io_out8(PIC1_ICW1, 0x11);   // エッジトリガモード
+  io_out8(PIC1_ICW2, 0x28);   // IRQ8-15 は INT28-2f で受け付ける
+  io_out8(PIC1_ICW3, 2);      // PIC1 は IRQ2 にて接続
+  io_out8(PIC1_ICW4, 0x01);   // ノンバッファモード
 
-  io_out8(PIC0_IMR, 0xfb);  // 11111011 PIC1 以外は全て禁止
-  io_out8(PIC1_IMR, 0xff);  // 11111111 全ての割り込みを受け付けない
+  io_out8(PIC0_IMR, 0xfb);    // 11111011 PIC1 以外は全て禁止
+  io_out8(PIC1_IMR, 0xff);    // 11111111 全ての割り込みを受け付けない
 
   return;
 }
