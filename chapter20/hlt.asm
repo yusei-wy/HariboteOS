ed3a3c17643c130e9e32a43da912c3fc034de218
yusei-wy <yusei.kasa@gmail.com>
2018-09-01 19:24:42 +0900

    Add アプリケーションの終了(harib17d)

--- chapter20/hlt.asm
+++ chapter20/hlt.asm
@@ -1,6 +1,13 @@
 BITS 32
-    MOV     AL,'A'
-    CALL    2*8:0xca2   ; アプリはセグメント'1003*8', OS はセグメント'2*8'の中で動いている
-fin:
-    HLT
-    JMP     fin
+    ; アプリはセグメント'1003*8', OS はセグメント'2*8'の中で動いている
+    MOV     AL,'h'
+    CALL    2*8:0xca2
+    MOV     AL,'e'
+    CALL    2*8:0xca2
+    MOV     AL,'l'
+    CALL    2*8:0xca2
+    MOV     AL,'l'
+    CALL    2*8:0xca2
+    MOV     AL,'o'
+    CALL    2*8:0xca2
+    RETF
